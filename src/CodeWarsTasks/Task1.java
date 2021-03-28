9b75f48c7da9813e3d5ca311a7e50a20857d5f3c
ArseniyAsriyan <80623657+ArseniyAsriyan@users.noreply.github.com>
2021-03-28 16:02:58 +0300

    CodeWars task1

---
+++ src/CodeWarsTasks/Task1.java
@@ -0,0 +1,23 @@
+package CodeWarsTasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(testSort(123456789));
+    }
+    public static int testSort(int num) {
+
+        int temp = num;
+        String result = "";
+        while (temp > 0) {
+            result += String.valueOf(temp % 10);
+            temp /= 10;
+        }
+        if (num <= 0) {
+            return num;
+        } else {
+            temp = Integer.parseInt(result);
+        }
+        return temp;
+
+    }
+}
